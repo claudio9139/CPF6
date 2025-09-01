22e4a20f178b15decafa148af355c2eed3c24811
claudio9139 <xgordon1@gmail.com>
2025-08-31 22:58:03 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Ejercicio_for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             Suma de pares hasta N
+             Pedir un número entero N y mostrar la suma de todos los números pares desde 1 hasta N.
+
+           
+
+            int numero;
+            string entrada;
+            long sumaNumero = 0;
+ 
+            Console.WriteLine("Ingrese un numero entero N \n\n");
+            entrada = Console.ReadLine();
+            numero = int.Parse(entrada);
+
+            for (int i=0;i<=numero;i++)
+            {
+                if (i % 2 == 0)
+                {
+                   Console.WriteLine($"Numero par : {i}");
+                    sumaNumero += i;
+                }
+            }
+
+            Console.WriteLine($"La suma de los numeros pares es   : {sumaNumero}");
+            */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

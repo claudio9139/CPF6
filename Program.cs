fdd7d478198fbb6f819c7f90a1c5c5dc040be940
claudio9139 <xgordon1@gmail.com>
2025-08-31 23:21:01 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,46 +1,42 @@
-﻿namespace Contador_numeros_positivos
+﻿namespace Desafio_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            /*
 
-                Contador de números positivos
-               Pedir números enteros al usuario hasta que ingrese un número negativo. 
-                Al final, mostrar cuántos positivos ingresó.
+            decimal Numero_Decimal;
+            float Numero_Float;
+            double Numero_Double;
+            int numero_azar;
+            string palabra;
+            palabra = "1e0d";
+         Random numero_aleat = new Random();
+         numero_azar = numero_aleat.Next();  
+         Console.WriteLine(numero_azar);
+            Numero_Float = numero_azar/10000000;
+            Numero_Decimal = 1e-1M;
+            //Numero_Float = .1f;
+            Numero_Double = 0.1d;
+            Console.WriteLine($"****************************{Numero_Float}");
+            for (int i = 0; i < Numero_Float/1; i++)
 
-            */
-
-            Console.WriteLine("Ingrese numeros enteros positivos, para finalizar ingrese un numero negativo \n\n");
-            int numero;
-            int contador = 0;
-            do
             {
-                numero = int.Parse(Console.ReadLine());
-                if (numero >= 0)
-                {
-                    contador++;
-                    Console.WriteLine($"Cardinal de numero ingresado {contador}   Numero ingresado {numero} \n\n");
-                }
-            } while (numero >= 0);
-            Console.WriteLine($"Ha ingresado {contador} numeros positivos");
-            Console.WriteLine("\n\n Fin del programa");
 
+                Numero_Decimal += .1M;
+                Numero_Float += .1f;
+                Numero_Double += .1d;
+                Console.WriteLine(i);
+                Console.WriteLine($"Decimal   { Numero_Decimal}\n");
+                Console.WriteLine($"Float     {Numero_Float    }\n");
+                Console.WriteLine($"Double    {Numero_Double   }\n\n\n");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+            }
+            
+            
+            
+            
+            
             //Console.WriteLine("Hello, World!");
         }
     }
